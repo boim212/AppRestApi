1654f0f0cef09de9130b14bc63aa1a1821ecb6e7
boim212 <bimaboim7@gmail.com>
2021-07-19 17:32:09 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req,res){
     response.ok("Aplikasi Rest Api Berjalan",res)
@@ -14,6 +15,21 @@ exports.tampilsemuadatasiswa = function(req,res){
             connection.log(error);
         }else{
             response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',[id],
+    
+    function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else{
+        response.ok(rows, res);
         }
     });
 };
--- routes.js
+++ routes.js
@@ -8,4 +8,7 @@ module.exports = function(app){
 
     app.route('/tampil')
         .get(jsonku.tampilsemuadatasiswa);
+    
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 };
